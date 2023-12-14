629bcd947e5764d1359fd6e53debf197d46005d4
Ahmed Elwerdany <infoalwerdany@gmail.com>
2023-12-14 07:36:43 +0200

    remove flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// 2 - router
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -16,9 +14,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	flag.Parse()
 	log.Println("Server Listinig on http://localhost:8080")
-	// 3 - server
 	err := http.ListenAndServe(":8080", mux)
 
 	log.Fatal(err)
